cc226d185d0c059a3cfc45df64fb366c40e5f64a
Walid Hawari <walidhawari95@gmail.com>
2024-08-07 11:18:47 +0300

    [UPDATE]: added figtree font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      figtree: ["Figtree", "sans-serif"],
+    },
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      yellow: "hsl(47, 88%, 63%)",
+      gray: {
+        500: "hsl(0, 0%, 42%)",
+        950: "hsl(0, 0%, 7%)",
+      },
+    },
   },
   plugins: [],
 };
